0e0d0cd5649d5cf6ff91fb1c617ee73d067a2c66
tianreformis <tianreformis@gmail.com>
2024-05-21 12:02:37 +0800

    commit : adding new audio file

--- app/image/page.tsx
+++ app/image/page.tsx
@@ -1,25 +1,18 @@
 import Image from "next/image";
 import bgImage from './bg.jpg';
+
 import imagaPlaceholder from '/public/love.svg';
 import { flushAllTraces } from "next/dist/trace";
 import React, { useState, useEffect } from "react";
-import useSound from 'use-sound';
-import mySound from '/song.mp3';
-
 
 
 export default function Page() {
-
-
-
     return (
-        <div className="flex flex-row items-center justify-center">
-            <span className="font-bold text-2xl bg-red-200 text-black p-4">I Love You</span>
-
-
+        <div className="flex flex-col items-center justify-center">
+            <p className="font-bold text-2xl bg-red-200 text-black p-4">I Love You</p>
+            <audio src="song-bg.mp3" controls autoPlay></audio>
 
         </div>
-
     )
 }
 
