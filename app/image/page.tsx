1f78b17725362f6290b9116b2f33c33f7d661c02
tianreformis <tianreformis@gmail.com>
2024-05-21 13:25:18 +0800

    commit : resolve the error on production

--- app/image/page.tsx
+++ app/image/page.tsx
@@ -2,9 +2,7 @@ import Image from "next/image";
 import bgImage from './bg.jpg';
 import imagePlaceholder from '/public/love.svg';
 
-
 export default function Page() {
-
     return (
         <div className="flex flex-col items-center justify-center">
             <p className="font-bold text-2xl bg-red-200 text-black p-4">I Love You</p>
