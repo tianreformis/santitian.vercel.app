e8e94454d222c4982c810a2e6d889ad1717f219a
tianreformis <tianreformis@gmail.com>
2024-05-21 14:08:39 +0800

    add file: song-bg.ogg

--- app/image/page.tsx
+++ app/image/page.tsx
@@ -2,12 +2,36 @@ import Image from "next/image";
 import bgImage from './bg.jpg';
 import imagePlaceholder from '/public/love.svg';
 
+"use client";
+import React, { useState, useEffect } from "react";
+
+const useAudio = (url: string) => {
+    const [audio] = useState(new Audio(url));
+    const [playing, setPlaying] = useState(false);
+
+    const toggle = () => {
+        setPlaying(!playing);
+        playing ? audio.play() : audio.pause();
+    };
+
+    useEffect(() => {
+        audio.addEventListener('ended', () => setPlaying(false));
+        return () => {
+            audio.removeEventListener('ended', () => setPlaying(false));
+        };
+    }, []);
+
+    return [playing, toggle];
+};
+
 export default function Page() {
+    const [playing, toggle] = useAudio("./song-bg.ogg");
+
     return (
         <div className="flex flex-col items-center justify-center">
             <p className="font-bold text-2xl bg-red-200 text-black p-4">I Love You</p>
-            <audio className="pt-10" src="song-bg.mp3" typeof="audio/mpeg" controls autoPlay>Play</audio>
+         
+            <button onClick={toggle} className="bg-white text-black font bold mt-7 p-4 rounded-lg">Play/Pause</button>
         </div>
-    )
+    );
 }
-
